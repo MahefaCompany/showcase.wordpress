98e3ca74f23c54fda17b06cde50c384169127fa4
mahasedra <mahasedra01@gmail.com>
2020-07-27 15:36:48 +0300

    essai ajouter menu et sous menu en back office

--- wp-content/plugins/mahasedraplugin/my_first_plugin.php
+++ wp-content/plugins/mahasedraplugin/my_first_plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Add Text To Footer
+Plugin Name: Essai de mes devs plugins
 */
  
 // Hook the 'wp_footer' action hook, add the function named 'mfp_Add_Text' to it
@@ -12,12 +12,71 @@ function mfp_Add_Text()
   echo "<p style='color: black;'>After the footer is loaded, my text is added!</p>";
 }
 
-function wporg_filter_title($title)
-{
-    return 'The ' . $title . ' was filtered';
+
+add_action( "admin_menu", "montheme_ajouter_menu_tableau_de_bord" );
+
+function montheme_ajouter_menu_tableau_de_bord() {
+
+   add_menu_page( 
+
+      __( "Mon thème - Configuration", "montheme" ), // texte de la balise <title>
+
+      __( "Mon thème", "montheme" ),  // titre de l'option de menu
+
+      "manage_options", // droits requis pour voir l'option de menu
+
+      "montheme_menu_admin", // slug
+
+      "montheme_creer_page_configuration" // fonction de rappel pour créer la page
+
+   );
+
 }
-add_filter('the_title', 'wporg_filter_title');
-add_action ('admin_footer', 'my_admin_footer_function');
-function my_admin_footer_function () {
-	echo "<p> Ceci sera inséré en bas de la page d'administration </p>";
+
+
+function montheme_creer_page_configuration() {
+
+  global $title;   // titre de la page du menu, tel que spécifié dans la fonction add_menu_page
+
+  ?>
+
+  <div class="wrap">
+
+     <h2><?php echo $title; ?></h2>   
+
+     ...
+
+  </div>
+
+  <?php
+
 }
+
+add_action( "admin_menu", "montheme_ajouter_sousmenu_tableau_de_bord" );
+
+function montheme_ajouter_sousmenu_tableau_de_bord(){
+  add_submenu_page( 
+
+  "montheme_menu_admin",  // slug du menu parent
+
+  __( "Mon thème - Mon sous-menu - Configuration", "montheme" ),  // texte de la balise <title>
+
+  __( "Mon sous-menu", "montheme" ),  // titre de l'option de sous-menu
+
+  "manage_options",  // droits requis pour voir l'option de menu
+
+  "montheme_sousmenu_admin", // slug
+
+  "montheme_sousmenu_creer_page_configuration"  // fonction de rappel pour créer la page
+
+);
+}
+
+
+function montheme_sousmenu_creer_page_configuration() {
+
+    echo "<div class='wrap'><h3>Sous menu1</h3></div>";
+
+}
+
+ 
