beb6b4f2648f9c28ccea60c904f9cca84e98123c
Mahefa Abel <abelmahefa@gmail.com>
2021-09-15 13:34:49 +0300

    Add plugin divers

---
+++ wp-content/plugins/woozone/melib/autoload-common.php
@@ -0,0 +1,15 @@
+<?php
+!defined('ABSPATH') and exit;
+
+require_once( WOOZONE_ABSPATH . 'composer_prefixed/guzzlehttp/vendor/scoper-autoload.php' );
+require_once( WOOZONE_ABSPATH . 'composer_prefixed/random-user-agent/vendor/scoper-autoload.php' );
+
+// functionalities
+require_once( WOOZONE_ABSPATH . 'melib/Utils.php' );
+require_once( WOOZONE_ABSPATH . 'melib/WooMisc.php' );
+
+require_once( WOOZONE_ABSPATH . 'melib/GeoLocation.php' );
+require_once( WOOZONE_ABSPATH . 'melib/GeoLocationStats.php' );
+
+require_once( WOOZONE_ABSPATH . 'melib/CountryAvailability.php' );
+require_once( WOOZONE_ABSPATH . 'melib/CountryAvailabilityStats.php' );
